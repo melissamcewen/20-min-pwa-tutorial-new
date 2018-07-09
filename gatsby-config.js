442807e520eaefff0c6d10bf889aba7d36d8b1f6
Melissa Mcewen <mgmcewen@gmail.com>
2018-07-09 07:43:29 -0500

    adding manifest and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: "GatsbyJS",
+      short_name: "GatsbyJS",
+      start_url: "/",
+      background_color: "#6b37bf",
+      theme_color: "#6b37bf",
+      display: "minimal-ui",
+      icon: "src/images/icon.png", // This path is relative to the root of the site.
+    },
+  },
+
+  ],
 }
